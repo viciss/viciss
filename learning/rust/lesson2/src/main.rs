878c8b90c122d92b21edd6e3733ef73727215f12
stone <nonsense.stone@gmail.com>
2016-04-20 23:20:05 +0800

    step-2
    1. add guess.cmp to compare two values.
    2. overide guess with u32 type and parse old string value to number.

--- learning/rust/lesson2/src/main.rs
+++ learning/rust/lesson2/src/main.rs
@@ -1,6 +1,7 @@
 extern crate rand;
 
 use std::io;
+use std::cmp::Ordering;
 use rand::Rng;
 
 fn main() {
@@ -8,7 +9,7 @@ fn main() {
 
     let secret_number = rand::thread_rng().gen_range(1, 101);
     println!("The secret number is: {}", secret_number);
-    
+
     println!("Please input your guess:");
 
     let mut guess = String::new();
@@ -17,5 +18,15 @@ fn main() {
     	.ok()
     	.expect("Failed to read line!");
 
+    let guess: u32 = guess.trim().parse()
+    	.ok()
+    	.expect("Please type a number!");
+    	
     println!("You guessed: {}", guess);
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!")
+    }
 }
