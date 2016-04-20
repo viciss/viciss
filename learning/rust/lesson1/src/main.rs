d9d0d9766f83aeca3b1e62166a927c46cf8b4e87
stone <nonsense.stone@gmail.com>
2016-04-20 22:02:34 +0800

    lesson1
    sample codes for rust.

---
+++ learning/rust/lesson1/src/main.rs
@@ -0,0 +1,16 @@
+fn main() {
+    let program = "+ + * - /";
+    let mut accumulator = 0;
+
+    for token in program.chars() {
+    	match token {
+    	    '+' => accumulator += 1,
+    	    '-' => accumulator -= 1,
+    	    '*' => accumulator *= 2,
+    	    '/' => accumulator /= 2,
+    	    _ => {}
+    	}
+    }
+    println!("The program \"{}\" calculates the value {}", 
+    	program, accumulator );
+}
