3957c7147bba6ebfeb03e04bc4ce2b90025e3fd0
stone <nonsense.stone@gmail.com>
2016-04-22 11:26:20 +0800

    step-2
    Add thread related codes.

--- learning/rust/lesson3/src/main.rs
+++ learning/rust/lesson3/src/main.rs
@@ -1,3 +1,5 @@
+use std::thread;
+
 struct Philosopher {
 	name: String,
 }
@@ -10,6 +12,8 @@ impl Philosopher {
 	}
 
 	fn eat(&self) {
+		println!("{} is eating.", self.name);
+		thread::sleep_ms(1000);
 		println!("{} is done eating.", self.name);
 	}
 }
@@ -23,7 +27,13 @@ fn main() {
 		Philosopher::new("Michel Foucault")
 	];
 
-	for p in &philosophers {
-		p.eat();
+	let handles: Vec<_> = philosophers.into_iter().map(|p| {
+		thread::spawn(move || {
+			p.eat();
+		})
+	}).collect();
+
+	for h in handles {
+		h.join().unwrap();
 	}
 }
