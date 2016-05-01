f8b3084169499eb2b56dc15b86b6010f555df061
stone <nonsense.stone@gmail.com>
2016-05-01 21:06:51 +0800

    test using tx/rx
    The comile result is failed, didn't find the reason yet.

--- learning/rust/sync/src/main.rs
+++ learning/rust/sync/src/main.rs
@@ -1,23 +1,18 @@
 use std::thread;
-use std::sync::{Arc, Mutex};
 use std::sync::mpsc;
 
 fn main() {
-	let data = Arc::new(Mutex::new(0u32));
 	let (tx, rx) = mpsc::channel();
 
 	for _ in 0..10 {
-		let (data, tx) = (data.clone(), tx.clone());
+		let tx = tx.clone();
 
 		thread::spawn(move || {
-			let mut data = data.lock().unwrap();
-			*data += 1;
+			let answer = 42u32;
 
-			tx.send(());
+			tx.send(answer);
 		});
 	}
 
-	for _ in 0..10 {
-		rx.recv();
-	}
+	rx.recv().ok().expect("Could not receive answer.")
 }
