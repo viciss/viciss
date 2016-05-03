9f8dfbdce9d8ac38e55108175b3aaa0a3b6c358d
stone <nonsense.stone@gmail.com>
2016-05-03 22:56:12 +0800

    wrong version of mutable variable

--- learning/rust/concept/src/main.rs
+++ learning/rust/concept/src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+	// define a variable
+	let x = 5;
+	// this is wrong!
+	// coz x is immutable
+	x = 10;
+
+	// define a tuple
+	let (y,z) = (1,2);
+
+	// define a variable with type
+	let a: i32 =5;
 }
