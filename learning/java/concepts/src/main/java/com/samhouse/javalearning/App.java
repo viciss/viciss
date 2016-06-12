583a784b77b93b3fb86f18035b6244eb640d2367
stone <nonsense.stone@gmail.com>
2016-06-12 21:38:11 +0800

    Initial a java project for learning purpose with maven: mvn -B archetype:generate -DarchetypeGroupId=org.apache.maven.archetypes -DgroupId=com.samhouse.javalearning -DartifactId=concepts

---
+++ learning/java/concepts/src/main/java/com/samhouse/javalearning/App.java
@@ -0,0 +1,13 @@
+package com.samhouse.javalearning;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
