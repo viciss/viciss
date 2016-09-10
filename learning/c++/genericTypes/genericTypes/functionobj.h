65e4f5bc7a17048df34511882a2dbe116baa65f9
Huang Yu <YU.HUANG@HPE.COM>
2016-09-10 10:13:11 +0800

    Refresh memory of function object.

--- learning/c++/genericTypes/genericTypes/functionobj.h
+++ learning/c++/genericTypes/genericTypes/functionobj.h
@@ -1,10 +1,41 @@
 #pragma once
-class functionobj
+template <class Type>
+class MultValue
 {
-public:
-	functionobj();
-	~functionobj();
+private:
+	Type Factor;
 
-	int operator()();
+public:
+	MultValue(const Type & _Val) : Factor(_Val)
+	{
+	}
+
+	void operator ()(Type & elem) const
+	{
+		elem *= Factor;
+	}
+};
+
+class Average
+{
+private:
+	long num;
+	long sum;
+
+public:
+	Average() : num(0), sum(0)
+	{
+	}
+
+	void operator () (int elem)
+	{
+		num++;
+		sum += elem;
+	}
+
+	operator double()
+	{
+		return static_cast<double> (sum) / static_cast<double> (num);
+	}
 };
 
--- learning/c++/genericTypes/genericTypes/main.cpp
+++ learning/c++/genericTypes/genericTypes/main.cpp
@@ -1,6 +1,48 @@
 #include "functionobj.h"
+#include <vector>
+#include <iostream>
+#include <algorithm>
 
 int main(int argc, char * argv[])
 {
+	using namespace std;
+	vector<int> v1;
+	vector<int>::iterator Iter1;
+
+	int i;
+	for (i = -4; i <= 2; i++)
+	{
+		v1.push_back(i);
+	}
+
+	cout << "Original vector v1 = ( ";
+	for (Iter1 = v1.begin(); Iter1 != v1.end(); Iter1++)
+	{
+		cout << *Iter1 << " ";
+	}
+	cout << ")." << endl;
+
+	for_each(v1.begin(), v1.end(), MultValue<int>(-2));
+
+	cout << "Multiplying the elements of the vector v1\n "
+		<< "by the factor -2 gives:\n v1mod1 = ( ";
+	for (Iter1 = v1.begin(); Iter1 != v1.end(); Iter1++)
+	{
+		cout << *Iter1 << " ";
+	}
+	cout << ")." << endl;
+
+	for_each(v1.begin(), v1.end(), MultValue<int>(5));
+	cout << "Multiplying the elements of the vector v1\n "
+		<< "by the factor 5 gives:\n v1mod1 = ( ";
+	for (Iter1 = v1.begin(); Iter1 != v1.end(); Iter1++)
+	{
+		cout << *Iter1 << " ";
+	}
+	cout << ")." << endl;
+
+	double avemod2 = for_each(v1.begin(), v1.end(), Average());
+	cout << "The average of the elements of v1 is: \n Average( v1mod2 ) = "
+		<< avemod2 << "." << endl;
 	return 0;
 }
