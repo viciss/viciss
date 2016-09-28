91fe7449c8e96af85ff1c512112d437ddb3e2121
Huang Yu <YU.HUANG@HPE.COM>
2016-09-28 09:06:54 +0800

    Add test codes for blockingqueue.

--- learning/java/concepts/src/main/java/com/samhouse/javalearning/example14/unsynch/Bank.java
+++ learning/java/concepts/src/main/java/com/samhouse/javalearning/example14/unsynch/Bank.java
@@ -7,8 +7,6 @@ import java.util.concurrent.locks.ReentrantLock;
 public class Bank
 {
   private final double[] accounts;
-  private Lock bankLock;
-  private Condition sufficientFunds;
 
   public Bank(int n, double initialBalance)
   {
@@ -17,47 +15,29 @@ public class Bank
     {
       accounts[i] = initialBalance;
     }
-    bankLock = new ReentrantLock();
-    sufficientFunds = bankLock.newCondition();
   }
 
-  public void transfer(int from, int to, double amount) throws InterruptedException
+  public synchronized void transfer(int from, int to, double amount) throws InterruptedException
   {
-    bankLock.lock();
-    try
-    {
-      while (accounts[from] < amount)
-        sufficientFunds.await();
+    while (accounts[from] < amount)
+      wait();
 
-      System.out.print(Thread.currentThread());
-      accounts[from] -= amount;
-      System.out.printf(" %10.2f from %d to %d", amount, from, to);
-      accounts[to] += amount;
-      System.out.printf(" Total Balance: %10.2f\n", getTotalBalance());
-      sufficientFunds.signalAll();
-    }
-    finally
-    {
-      bankLock.unlock();
-    }
+    System.out.print(Thread.currentThread());
+    accounts[from] -= amount;
+    System.out.printf(" %10.2f from %d to %d", amount, from, to);
+    accounts[to] += amount;
+    System.out.printf(" Total Balance: %10.2f\n", getTotalBalance());
+    notifyAll();
   }
 
-  public double getTotalBalance()
+  public synchronized double getTotalBalance()
   {
-    bankLock.lock();
-    try
-    {
-      double sum = 0;
+    double sum = 0;
 
-      for (double a : accounts)
-        sum += a;
+    for (double a : accounts)
+      sum += a;
 
-      return sum;
-    }
-    finally
-    {
-      bankLock.unlock();
-    }
+    return sum;
   }
 
   public int size()
