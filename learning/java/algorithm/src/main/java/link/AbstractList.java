83cea69b4e25b5a750b54ab4a9aebd51ba8f3b72
stone <nonsense.stone@gmail.com>
2016-11-10 22:22:17 +0800

    Refactor ArrayList.

--- learning/java/algorithm/src/main/java/link/AbstractList.java
+++ learning/java/algorithm/src/main/java/link/AbstractList.java
@@ -1,11 +1,13 @@
 package link;
 
+import java.util.function.IntFunction;
+
 public interface AbstractList<T>
 {
-  AbstractList Initialize();
-  T FindKth(int k, AbstractList list);
-  int Find(T element, AbstractList list);
-  void Insert(T element, int i, AbstractList list);
-  void Delete(int i, AbstractList list);
-  int Length(AbstractList list);
+  void Initialize(IntFunction<T[]> arrayConstructor, int size);
+  T FindKth(int k);
+  int Find(T element);
+  void Insert(T element, int i);
+  void Delete(int i);
+  int Length();
 }
